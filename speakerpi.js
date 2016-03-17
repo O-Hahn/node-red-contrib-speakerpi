e5a15c7c272e97b97d44fb236bc7e4e5946dbd93
analytics <analytics@10.33.5.147>
2016-03-17 17:48:09 +0100

    speaker with default wav selections

--- speakerpi.js
+++ speakerpi.js
@@ -35,83 +35,112 @@ function speakOutput(outStream, speakerConfig) {
 	rs.push(outStream);
 	rs.push(null);
 	
-	rs.on("end", function () {
-	    speaker.on("finish",function () {
-	    	console.log("Speaker-Out: I am now finished");
-	    });
-
-	    speaker.on("close",function () {
-	    	console.log("Speaker-Out: I am now closed");
-	    });
-
-	    // send file to output
-	    rs.pipe(speaker);
-		
-	});
- 	    
+    // send file to output
+    rs.pipe(speaker);
+	console.log("SpeakerPi (log): Sound is send to speaker.");	
+	    
     return;
 };
 
-function speakOutputFile(outStream) {
+function speakOutputFile(outStream,fileopt) {
 	var Sound = require('node-aplay');
  	var fs = require("fs-extra");
  	var os = require("os");
  	var uuid = require('node-uuid');
  	
  	var uuid = uuid.v4();
- 	var filename = "/home/pi/.node-red/speak/speak-" + uuid +".wav";
+ 	var filename = "";
   	
 	var data = outStream;
- 		
- 	console.log("SpeakOutputFile:"+ filename);
  	
-	if ((typeof data === "object") && (!Buffer.isBuffer(data))) {
- 			 data = JSON.stringify(data);
+ 	// define the standard sounds if set or temp filename
+    if (fileopt) {
+    	if(fileopt == "watson-1") {
+    		filename = "/home/pi/.node-red/default/watson-1.wav";  
+    	} else if(fileopt == "watson-2") {
+    		filename = "/home/pi/.node-red/default/watson-2.wav";
+    	} else if(fileopt == "watson-3") {
+    		filename = "/home/pi/.node-red/default/watson-3.wav";  
+    	} else if(fileopt == "watson-4") {
+    		filename = "/home/pi/.node-red/default/watson-4.wav";  
+    	} else if(fileopt == "watson-5") {
+    		filename = "/home/pi/.node-red/default/watson-5.wav";
+    	} else {
+    		filename = "/home/pi/.node-red/speak/speak-" + uuid +".wav"; 
+    	}
+    } else {
+     	filename = "/home/pi/.node-red/speak/speak-" + uuid +".wav";          
+    }
+    
+    console.log("SpeakOutputFile:"+ filename);
+
+    // write soundobject to file 
+ 	if (fileopt && fileopt == "payload") {
+ 		if ((typeof data === "object") && (!Buffer.isBuffer(data))) {
+			 data = JSON.stringify(data);
 		}
-    if (typeof data === "boolean") { data = data.toString(); }
-    if (typeof data === "number") { data = data.toString(); }
-    if (!Buffer.isBuffer(data)) { data += os.EOL; }
-         
-    data = new Buffer(data);
-          
-     // using "binary" not {encoding:"binary"} to be 0.8 compatible for a while
-     fs.writeFile(filename, data, "binary", function (err) {
-         if (err) {
-             if (err.code === "ENOENT") {
-                 fs.ensureFile(filename, function (err) {
-                     if (err) { 
-                     	console.error("Speaker-Out:  (err): File "+ filename + " could not be created");
-                     }
-                     else {
-                         fs.writeFile(filename, data, "binary", function (err) {
-                             if (err) { 
-                             	console.error("Speaker-Out:  (err): File " + filename + " could not be written to");
-                             	}
-                         });
-                     }
-                 });
-             }
-             else { 
-             	console.error("Speaker-Out: (err): error writing " + err);
-             }
-         }
-         else { 
-         	console.log("Speaker-Out: (log): File " + filename + " written.");
+	   if (typeof data === "boolean") { data = data.toString(); }
+	   if (typeof data === "number") { data = data.toString(); }
+	   if (!Buffer.isBuffer(data)) { data += os.EOL; }
+	        
+	   data = new Buffer(data);
+	         
+	    // using "binary" not {encoding:"binary"} to be 0.8 compatible for a while
+	    fs.writeFile(filename, data, "binary", function (err) {
+	        if (err) {
+	            if (err.code === "ENOENT") {
+	                fs.ensureFile(filename, function (err) {
+	                    if (err) { 
+	                    	console.error("SpeakerPi (err): File "+ filename + " could not be created");
+	                    	return;
+	                    }
+	                    else {
+	                        fs.writeFile(filename, data, "binary", function (err) {
+	                            if (err) { 
+	                            	console.error("SpeakerPi (err): File " + filename + " could not be written to");
+	                            	return;
+	                            	}
+	                        });
+	                    }
+	                });
+	            }
+	            else { 
+	            	console.error("SpeakerPi (err): error writing " + err);
+	            	return;
+	            }
+	        } else {
+	        	console.log("SpeakerPi (log): File " + filename + " written.");
+	        	
+	        	// speak out the streamed file or the standard file 
+	           	var speak = new Sound(filename);
+	           	speak.play();
+	           	
+	           	speak.on('complete', function () {
+	           		console.log('SpeakerPi (log): Done with playback!');
 
-         	// fire and forget: 
-        	var speak = new Sound(filename).play();
-        	
-        	speak.on('complete', function () {
-        		console.log('Speaker-Out (log): Done with playback!');
+	           		// delete file - if payload given and tempfile is not needed anymore
+	           		if (fileopt && fileopt == "payload") {
+	           	   		fs.remove(filename, function(err) {
+	                 		  if (err) return console.error("SpeakerPi (err): "+ err);
+	                 		  
+	                 		  console.log("SpeakerPi (log): remove success!")
+	                 		});	           			
+	           		}
+	           	});
+	        	}
+	    });
+ 	} else {
+ 		// speak out the streamed file or the standard file 
+ 	   	var speak = new Sound(filename);
+ 	   	speak.play();
+ 	   	
+ 	   	speak.on('complete', function () {
+ 	   		console.log('SpeakerPi (log): Done with playback!');
+ 	   	});
 
-        		fs.remove(filename, function(err) {
-            		  if (err) return console.error("Speaker-Out (err): "+ err);
-            		  
-            		  console.log("Speaker-Out (log): remove success!")
-            		});
-        	});
-         	}
-     });
+ 	}
+   	
+ 	return;
 };
 
 
@@ -133,6 +162,7 @@ module.exports = function(RED) {
 		this.bitdepth =  config.bitdepth;
 		this.samplerate =  config.samplerate;
 		this.choose = config.choose;
+		this.predefsound = config.predefsound;
 		this.name =  config.name;
 
 		var node = this;
@@ -141,15 +171,19 @@ module.exports = function(RED) {
         node.on("input",function(msg) {
             node.status({fill:"green",shape:"dot",text:"node-red:common.status.connected"});
 
+
             // check if speech is filled or standard-sound given
 			if (msg.speech) {
+				if(msg.predefsound) {
+					this.predefsound = msg.predefsound;
+				}
 				var speakerConfig = {
 					channels: node.channels,          // 2 channels
 					bitdepth: node.bitdepth,          // 16-bit samples
 					samplesate: node.samplerate       // 44,100 Hz sample rate
 				};
 				
-				if (node.choose == "filebased") { speakOutputFile(msg.speech); }
+				if (node.choose == "filebased") { speakOutputFile(msg.speech,node.predefsound); }
 				else { speakOutput(msg.speech, speakerConfig);  }
 				   					
 			} else {
